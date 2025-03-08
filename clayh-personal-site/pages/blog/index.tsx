f0e074bc812cbb9b6ca9fa43ba8d612ccecf8617
Henry Clay <mr.henry.clay@gmail.com>
2025-03-08 18:37:37 -0500

    starting API for space stuff

--- clayh-personal-site/pages/blog/index.tsx
+++ clayh-personal-site/pages/blog/index.tsx
@@ -26,7 +26,6 @@ export default function Blog() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
       <GradientBG 
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`} 
         lightSize={`60`}
