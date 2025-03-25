4f23c566b90329c1fa3dda0e132fd2d90e9124a2
Henry Clay <mr.henry.clay@gmail.com>
2025-03-25 12:14:21 -0400

    analytics main 2

--- clayh-personal-site/components/solarSystem.tsx
+++ clayh-personal-site/components/solarSystem.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from "@/styles/Home.module.css";
-import Link from 'next/link';
+//import Link from 'next/link';
 
 type SystemProps = {
     title: string,
